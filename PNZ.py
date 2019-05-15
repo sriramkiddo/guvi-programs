ec2f76b1e1adf0f5fc2f752da6a51ebcbefe6caa
sriramkiddo <50619552+sriramkiddo@users.noreply.github.com>
2019-05-15 08:16:53 +0530

    Rename pnz.py to PNZ.py

---
+++ PNZ.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number"))
+if(num<0):
+    print("negative")
+elif(num==0):
+    print("zero")
+elif(num>0):
+    print("positive")
+else:
+    print("enter a valid number")
