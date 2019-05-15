8fa456c81c0308008afd36f3b7bfb7c1388b0bd2
sriramkiddo <50619552+sriramkiddo@users.noreply.github.com>
2019-05-15 09:05:34 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,5 @@
 character=input()
 if(character=='a' or character=='e' or character=='i' or character=='o' or character=='u' or character=='A' or character=='E' or character=='I' or character=='O' or character=='U'):
-    print(Vowel)
+    print("Vowel")
 else:
     print("Consonant")
