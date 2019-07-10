d8e3f6074d6039998af87e592b18615aee04fb2d
sriramkiddo <50619552+sriramkiddo@users.noreply.github.com>
2019-07-10 20:28:10 +0530

    Update AtoZ.py

--- AtoZ.py
+++ AtoZ.py
@@ -1,11 +1,11 @@
-k,l=map(str,input().split())
-S=0
-if len(k)>len(l):
-    k,l=l,k
+cha,cho=map(str,input().split())
+yas=0
+if len(cha)>len(cho):
+  cha,cho=cho,cha
 p=0
-while p<len(k):
-      S+=(ord(l[p])-ord(k[p]))
-      p+=1
-for p in range(p,len(k)):
-      S+=ord(k[p])-ord('a')+1
-print(S)
+while p<len(cha):
+  yas+=(ord(cho[p])-ord(cha[p]))
+  p+=1
+for p in range(p,len(cho)):
+  yas+=ord(cho[p])-ord('a')+1
+print(yas)
