605c3d82113c7861818a71a54f399f32bd1edfd1
sriramkiddo <50619552+sriramkiddo@users.noreply.github.com>
2019-07-10 20:22:41 +0530

    Update AtoZ.py

--- AtoZ.py
+++ AtoZ.py
@@ -1,11 +1,11 @@
 k,l=map(str,input().split())
-s=0
+sol=0
 if len(k)>len(l):
     k,l=l,k
 p=0
 while p<len(k):
-      s+=(ord(l[p])-ord(k[p]))
+      sol+=(ord(l[p])-ord(k[p]))
       p+=1
 for p in range(p,len(k)):
-      s+=ord(k[p])-ord('a')+1
-print(s)
+      sol+=ord(k[p])-ord('a')+1
+print(sol)
