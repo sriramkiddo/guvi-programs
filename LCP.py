269c6743d55a2c4bf4d2dc8b4f039c365170cf93
sriramkiddo <50619552+sriramkiddo@users.noreply.github.com>
2019-07-10 19:31:45 +0530

    Update LCP.py

--- LCP.py
+++ LCP.py
@@ -1,12 +1,12 @@
 pt1=int(input())
 ot1=[]
-for hr in range(0,pt1):
+for hr1 in range(0,pt1):
  pant1=input()
  ot1.append(pant1)
 de=[]
 for hr1 in zip(*ot1):
  if(hr1.count(hr1[0])==len(hr1)):
-  de.append(hr[0])
+  de.append(hr1[0])
  else:
   break
 print(''.join(de))
