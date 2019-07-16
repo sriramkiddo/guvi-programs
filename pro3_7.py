2c0c5f07fd8530756724d9b21e1ca55f90aa367d
sriramkiddo <50619552+sriramkiddo@users.noreply.github.com>
2019-07-16 20:48:31 +0530

    Update pro3_7.py

--- pro3_7.py
+++ pro3_7.py
@@ -1,19 +1,17 @@
-n13,w13=map(int,input().split())
-weights=list(map(int,input().split()))
-val13=list(map(int,input().split()))
-r=[]
-for i in range(n13):
-    if(weights[i]>w13):
-        del weights[i]
-        del val13[i]
-n13=len(val)
-for i in range(n13):
-    r.append(val13[i]/weights[i])
-sum=0
-tot=0
-l=sorted(zip(r,val13,weights),reverse=True)
-for _,x,y in l:
-    if(sum+y<=w13):
-        sum+=y
-        tot+=x
-print(tot)
+A13,B13=map(int,input().split())
+C13=list(map(int,input().split()))
+pr=list(map(int,input().split()))
+qr=[]
+ar=0
+for i in range(A13):
+    x=pr[i]/C13[i]
+    qr.append(x)
+while B13>=0 and len(qr)>0:
+    mindex=qr.index(max(qr))
+    if B13>=C13[mindex]:
+        ar=ar+pr[mindex]
+        B=B-C13[mindex]
+    C13.pop(mindex)
+    pr.pop(mindex)
+    qr.pop(mindex)
+print(ar)
